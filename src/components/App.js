da46b2edd0b9f5e77d717186b78a1e7d621c946e
Bartosz Cytrowski <bartosz.cytrowski@gmail.com>
2017-07-04 11:42:36 +0200

    Using state

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,8 @@ import Counter from './Counter'
 export default class App extends React.Component {
   render() {
     return (
-      <div
-        id="foo"
-        className="welcome"
-      >
-        <Counter initialValue="2"/>
-        <Counter initialValue={4}/>
-        <Counter initialValue={true}/>
-        <Counter initialValue="true"/>
+      <div>
+        <Counter />
       </div>
     )
   }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,29 @@
 import React from 'react'
 
 export default class Counter extends React.Component {
+
+  state = {
+    value: 0
+  }
+
+  handleIncrementClick = () => {
+    this.setState({
+      value: this.state.value + 1
+    })
+  }
+
   render() {
     return (
       <div>
-        <h1>Counter - {this.props.initialValue}</h1>
-        <strong>{typeof this.props.initialValue}</strong>
+        <h1>Counter</h1>
+        <h2>{this.state.value}</h2>
+        <div>
+          <button
+            onClick={this.handleIncrementClick}
+          >
+            Increase
+          </button>
+        </div>
       </div>
     )
   }
