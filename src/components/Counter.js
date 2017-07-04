9b2cce169d5c3b8fea68204cbbe08e3199b31a8b
mkolasinski <mkolasinski@poczta.fm>
2017-07-04 11:56:01 +0200

    trzy przyciski

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,29 +2,52 @@ import React from 'react'
 
 export default class Counter extends React.Component {
 
-  state = {
-    value: 0
-  }
+    state = {
+        value: 0
+    }
 
-  handleIncrementClick = () => {
-    this.setState({
-      value: this.state.value + 1
+    dodaje = () => {
+        this.setState({
+            value: this.state.value + 1
+        })
+    }
+
+    odejmuje = () => {
+        this.setState({
+            value: this.state.value - 1
+        })
+    }
+
+    resetuje = () => {
+        this.setState({
+            value:0
     })
-  }
+    }
 
-  render() {
-    return (
-      <div>
-        <h1>Counter</h1>
-        <h2>{this.state.value}</h2>
-        <div>
-          <button
-            onClick={this.handleIncrementClick}
-          >
-            Increase
-          </button>
-        </div>
-      </div>
-    )
-  }
+    render() {
+      return (
+            <div>
+                <h1>Counter</h1>
+                <h2>{this.state.value}</h2>
+                <div>
+                    <button
+                        onClick={this.dodaje}
+                    >
+                        Increase
+                    </button>
+                    <button
+                        onClick={this.odejmuje}
+                    >
+                        Decrement
+                    </button>
+                    <button
+                        onClick={this.resetuje}
+                    >
+                        Reset
+                    </button>
+
+                </div>
+            </div>
+        )
+    }
 }
