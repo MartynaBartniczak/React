e9d0ae18050b965f0de0f9f73215fcdf5dfc68c3
Bartosz Cytrowski <bartosz.cytrowski@gmail.com>
2017-07-04 10:57:54 +0200

    First component

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = React.createClass({
+  render: function () {
+    return (
+      <div
+        id="foo"
+        className="welcome"
+      >
+        Hello
+      </div>
+    )
+  }
+})
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import App from './components/App'
+
 ReactDOM.render(
-  <div
-    id="foo"
-    className="welcome"
-  >
-    Hello
-  </div>,
+  <App />,
   document.getElementById('root')
 )
