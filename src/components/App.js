bdae06e2c34fd7ad16356096a2210a8cc9527c91
mkolasinski <mkolasinski@poczta.fm>
2017-07-05 11:48:44 +0200

    - sformulowanie wlasnego navbaru - import navbaru w MainMenu

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import {Nav, Navbar, MenuItem, NavDropdown, NavItem} from 'react-bootstrap'
+
 
 import Counter from './Counter'
+import MainMenu from './MainMenu'
 
 
 export default class App extends React.Component {
@@ -20,25 +21,8 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-          <Navbar>
-            <Navbar.Header>
-              <Navbar.Brand>
-                <a href="http://react-bootstrap.github.io">React-Bootstrap</a>
-              </Navbar.Brand>
-            </Navbar.Header>
-            <Nav>
-              <NavItem eventKey={1} href="#">Link</NavItem>
-              <NavItem eventKey={2} href="#">Link</NavItem>
-              <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                <MenuItem eventKey={3.1}>Action</MenuItem>
-                <MenuItem eventKey={3.2}>Another action</MenuItem>
-                <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                <MenuItem divider />
-                <MenuItem eventKey={3.4}>Separated link</MenuItem>
-              </NavDropdown>
-            </Nav>
-          </Navbar>
-          );
+          <MainMenu/>
+
         <Counter
           initialValue={1}
           handleZupa={this.increaseCommonCounter}
