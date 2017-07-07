974c2999d60ff26de4b99967a0b74b07c19a0115
Bartosz Cytrowski <bartosz.cytrowski@gmail.com>
2017-07-07 12:07:38 +0200

    Update menu

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -26,13 +26,12 @@ const MainMenu = (props) => (
       <LinkContainer to="/employees">
         <NavItem>Employees</NavItem>
       </LinkContainer>
-      <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-        <MenuItem eventKey={3.1}>Action</MenuItem>
-        <MenuItem eventKey={3.2}>Another action</MenuItem>
-        <MenuItem eventKey={3.3}>Something else here</MenuItem>
-        <MenuItem divider/>
-        <MenuItem eventKey={3.4}>The life of Zupa</MenuItem>
-      </NavDropdown>
+      <LinkContainer to="/students">
+        <NavItem>Students</NavItem>
+      </LinkContainer>
+      <LinkContainer to="/groups">
+        <NavItem>Groups</NavItem>
+      </LinkContainer>
     </Nav>
   </Navbar>
 )
