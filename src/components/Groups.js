f2992f32276f5327aef4c27d1868e4595e41d6c5
Bartosz Cytrowski <bartosz.cytrowski@gmail.com>
2017-07-07 12:27:44 +0200

    Groups and Students on one view

--- src/components/Groups.js
+++ src/components/Groups.js
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default class Groups extends React.Component {
 
+  state = {
+    groups: [],
+    students: []
+  }
+
   componentWillMount() {
     fetch(
       'http://localhost:3000/data/groups.json'
@@ -14,17 +19,37 @@ export default class Groups extends React.Component {
     ).catch(
       error => console.log(error.message)
     )
+
+    fetch(
+      'http://localhost:3000/data/students.json'
+    ).then(
+      response => response.json()
+    ).then(
+      data => this.setState({
+        students: data
+      })
+    ).catch(
+      error => console.log(error.message)
+    )
   }
 
   render() {
+    console.log(JSON.stringify(this.state))
     return (
       <div>
         <h1>Groups</h1>
         <ul>
-          { this.state === null ? <p>Fetching data ....</p> : null}
           {
             this.state !== null && this.state.groups.map(
               group => <li key={group.id}>{group.name}</li>
+            )
+          }
+        </ul>
+        <h1>Students</h1>
+        <ul>
+          {
+            this.state !== null && this.state.students.map(
+              student => <li key={student.id}>{student.name}</li>
             )
           }
         </ul>
