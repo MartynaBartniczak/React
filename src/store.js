dcefb5b5b71bce6e2efc28903b4bdd059577de93
Bartosz Cytrowski <bartosz.cytrowski@gmail.com>
2017-07-11 01:55:09 +0200

    Introduce redux-thunk

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
 
 import counter from './state/counter'
 
@@ -6,9 +7,11 @@ const reducer = combineReducers({
   counter
 })
 
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducer, /* preloadedState, */ composeEnhancers(
+  applyMiddleware(
+    thunk
+  )
+))
 
 export default store
