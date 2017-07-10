a4deb3dfe93a3b68572b31a61f3eda75df459640
Bartosz Cytrowski <bartosz.cytrowski@gmail.com>
2017-07-10 14:23:12 +0200

    Adding redux dev tools

--- src/store.js
+++ src/store.js
@@ -6,6 +6,9 @@ const reducer = combineReducers({
   counter
 })
 
-const store = createStore(reducer)
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 export default store
