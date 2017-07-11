13d7972bfe106e6398985b038400294efceea187
Bartosz Cytrowski <bartosz.cytrowski@gmail.com>
2017-07-11 10:00:39 +0200

    Students reducer

---
+++ src/state/students.js
@@ -0,0 +1,52 @@
+// action types
+const FETCH__BEGIN = 'students/FETCH__BEGIN'
+const FETCH__SUCCESS = 'students/FETCH__SUCCESS'
+const FETCH__FAIL = 'students/FETCH__FAIL'
+
+// action creators
+const fetchBegin = () => ({
+  type: FETCH__BEGIN
+})
+
+const fetchSuccess = data => ({
+  type: FETCH__SUCCESS,
+  data
+})
+
+const fetchFail = error => ({
+  type: FETCH__FAIL,
+  error
+})
+
+// initial state
+const initialState = {
+  data: null,
+  fetching: false,
+  error: null
+}
+
+// reducer
+export default (state = initialState, action = {}) => {
+  switch(action.type) {
+    case FETCH__BEGIN:
+      return {
+        ...state,
+        fetching: true,
+        error: null
+      }
+    case FETCH__SUCCESS:
+      return {
+        ...state,
+        fetching: false,
+        data: action.data
+      }
+    case FETCH__FAIL:
+      return {
+        ...state,
+        error: action.error,
+        fetching: false
+      }
+    default:
+      return state
+  }
+}
