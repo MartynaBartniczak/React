b3110bb38227592f9e7ddcb082b1e169b018b5fb
Bartosz Cytrowski <bartosz.cytrowski@gmail.com>
2017-07-12 13:55:00 +0200

    Search input

--- src/components/StudentSearcher.js
+++ src/components/StudentSearcher.js
@@ -2,8 +2,21 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { FormControl } from 'react-bootstrap'
 
-const StudentSearcher = () => (
-  <FormControl style={{marginBottom: 20}}/>
+import { updateSearchPhrase } from '../state/studentSearcher'
+
+const StudentSearcher = ({ searchPhrase, update }) => (
+  <FormControl
+    value={searchPhrase}
+    onChange={update}
+    style={{marginBottom: 20}}
+  />
 )
 
-export default connect()(StudentSearcher)
+export default connect(
+  state => ({
+    searchPhrase: state.studentSearcher.searchPhrase
+  }),
+  dispatch => ({
+    update: event => dispatch(updateSearchPhrase(event.target.value))
+  })
+)(StudentSearcher)
