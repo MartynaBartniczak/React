3a3389b1aee579397e21ccae84193f5370098839
Bartosz Cytrowski <bartosz.cytrowski@gmail.com>
2017-07-14 14:16:19 +0200

    Favorite students view

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,13 +1,43 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-export default connect(
+import { fetchStudents } from '../state/students'
 
+export default connect(
+  state => ({
+    availableStudents: state.students.data,
+    favoriteStudentIds: state.favoriteStudents.favoriteStudentIds
+  }),
+  dispatch => ({
+    fetchStudents: () => dispatch(fetchStudents())
+  })
 )(
   class Favorites extends React.Component {
+
+    componentWillMount() {
+      this.props.fetchStudents()
+    }
+
     render() {
       return (
-        <h1>Favorites</h1>
+        <div>
+          <h1>Favorites</h1>
+          <ul>
+            {
+              this.props.availableStudents !== null && this.props.availableStudents.filter(
+                student => this.props.favoriteStudentIds.includes(student.id)
+              ).map(
+                student => (
+                  <li key={student.id}>
+                    {
+                      student.name
+                    }
+                  </li>
+                )
+              )
+            }
+          </ul>
+        </div>
       )
     }
   }
